015f3e4bb8da6bcb7f00f5944cec770485329664
Wend-Kuuni <belemgnegreetienne@gmail.com>
2024-12-23 15:22:18 +0000

    Add custom text styles and enhance dashboard screen layout

--- lib/Screen/dash_board_screen.dart
+++ lib/Screen/dash_board_screen.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_clone/domain/custom_text_style.dart';
 import 'package:flutter/material.dart';
 
 class DashBoardScreen extends StatefulWidget {
@@ -10,6 +11,37 @@ class DashBoardScreen extends StatefulWidget {
 class _DashBoardScreenState extends State<DashBoardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'facebook',
+          style: myTextStyle30(
+              textColor: Colors.blueAccent, textWeight: FontWeight.bold),
+        ),
+        actions: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Icon(
+              Icons.add_sharp,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          const SizedBox(width: 5),
+          const Icon(
+            Icons.search_rounded,
+            size: 40,
+          ),
+          const SizedBox(width: 5),
+          Image.asset(
+            "assets/Icons/chat.png",
+            height: 30,
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/domain/custom_text_style.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+TextStyle myTextStyle15(
+    {Color textColor = Colors.black,
+    FontWeight textWeight = FontWeight.normal,
+    String fontFamily = "primary"}) {
+  return TextStyle(
+      color: textColor,
+      fontWeight: textWeight,
+      fontFamily: fontFamily,
+      fontSize: 15);
+}
+
+TextStyle myTextStyle20(
+    {Color textColor = Colors.black,
+    FontWeight textWeight = FontWeight.normal,
+    String fontFamily = "primary"}) {
+  return TextStyle(
+      color: textColor,
+      fontWeight: textWeight,
+      fontFamily: fontFamily,
+      fontSize: 20);
+}
+
+TextStyle myTextStyle22(
+    {Color textColor = Colors.black,
+    FontWeight textWeight = FontWeight.normal,
+    String fontFamily = "primary"}) {
+  return TextStyle(
+      color: textColor,
+      fontWeight: textWeight,
+      fontFamily: fontFamily,
+      fontSize: 22);
+}
+
+TextStyle myTextStyle25(
+    {Color textColor = Colors.black,
+    FontWeight textWeight = FontWeight.normal,
+    String fontFamily = "primary"}) {
+  return TextStyle(
+      color: textColor,
+      fontWeight: textWeight,
+      fontFamily: fontFamily,
+      fontSize: 25);
+}
+
+TextStyle myTextStyle28(
+    {Color textColor = Colors.black,
+    FontWeight textWeight = FontWeight.normal,
+    String fontFamily = "primary"}) {
+  return TextStyle(
+      color: textColor,
+      fontWeight: textWeight,
+      fontFamily: fontFamily,
+      fontSize: 28);
+}
+
+TextStyle myTextStyle30(
+    {Color textColor = Colors.black,
+    FontWeight textWeight = FontWeight.normal,
+    String fontFamily = "primary"}) {
+  return TextStyle(
+      color: textColor,
+      fontWeight: textWeight,
+      fontFamily: fontFamily,
+      fontSize: 30);
+}
