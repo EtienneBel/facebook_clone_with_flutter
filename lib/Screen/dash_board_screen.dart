6fa7fbc11cdf3e6d37bd9ec24841d9cbb6fe8536
Wend-Kuuni <belemgnegreetienne@gmail.com>
2024-12-23 18:51:39 +0000

    Add new screens for Home, Friends, Notifications, Profile, Store, and Video in the dashboard

--- lib/Screen/dash_board_screen.dart
+++ lib/Screen/dash_board_screen.dart
@@ -1,3 +1,9 @@
+import 'package:facebook_clone/Screen/Tabs/friends_screen.dart';
+import 'package:facebook_clone/Screen/Tabs/home_screen.dart';
+import 'package:facebook_clone/Screen/Tabs/notification_screen.dart';
+import 'package:facebook_clone/Screen/Tabs/profile_screen.dart';
+import 'package:facebook_clone/Screen/Tabs/store_screen.dart';
+import 'package:facebook_clone/Screen/Tabs/video_screen.dart';
 import 'package:facebook_clone/domain/custom_text_style.dart';
 import 'package:flutter/material.dart';
 
@@ -11,37 +17,71 @@ class DashBoardScreen extends StatefulWidget {
 class _DashBoardScreenState extends State<DashBoardScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'facebook',
-          style: myTextStyle30(
-              textColor: Colors.blueAccent, textWeight: FontWeight.bold),
-        ),
-        actions: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(20),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+          length: 6,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                'facebook',
+                style: myTextStyle30(
+                    textColor: Colors.blueAccent, textWeight: FontWeight.bold),
+              ),
+              actions: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Icon(
+                    Icons.add_sharp,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ),
+                const SizedBox(width: 5),
+                const Icon(
+                  Icons.search_rounded,
+                  size: 30,
+                ),
+                const SizedBox(width: 5),
+                Image.asset(
+                  "assets/Icons/chat.png",
+                  height: 20,
+                ),
+                const SizedBox(width: 5),
+              ],
+              bottom: TabBar(
+                  indicatorColor: Colors.blueAccent,
+                  labelColor: Colors.blueAccent,
+                  automaticIndicatorColorAdjustment: true,
+                  tabs: [
+                    Tab(icon: Icon(Icons.home_filled)),
+                    Tab(icon: Icon(Icons.video_collection)),
+                    Tab(icon: Icon(Icons.group)),
+                    Tab(icon: Icon(Icons.storefront)),
+                    Tab(icon: Icon(Icons.notifications)),
+                    Tab(
+                        icon: ClipOval(
+                      child: Image.asset(
+                        'assets/Images/girls3.jpg',
+                        height: 30,
+                        width: 40,
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                  ]),
             ),
-            child: const Icon(
-              Icons.add_sharp,
-              color: Colors.white,
-              size: 30,
-            ),
-          ),
-          const SizedBox(width: 5),
-          const Icon(
-            Icons.search_rounded,
-            size: 40,
-          ),
-          const SizedBox(width: 5),
-          Image.asset(
-            "assets/Icons/chat.png",
-            height: 30,
-          )
-        ],
-      ),
+            body: const TabBarView(children: [
+              ProfileScreen(),
+              HomeScreen(),
+              VideoScreen(),
+              FriendsScreen(),
+              StoreScreen(),
+              NotificationScreen(),
+            ]),
+          )),
     );
   }
 }
